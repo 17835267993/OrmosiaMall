a6db65f991cd15aeb87680ef6fa056f85af8decd
rainbow <1021109787@qq.com>
2020-10-16 21:12:14 +0800

    修改

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="tab-bar-items" @click="itemClick">
+    <div v-if="!isActive"><slot name="item-icon"></slot></div>
+    <div v-else><slot name="item-icon-active"></slot></div>
+    <div :style="activeStyle"><slot name="item-text"></slot></div>
+    
+    <!-- <img src="../../assets/img/tabbar/home.svg" alt="">
+    <div>首页</div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TabBarItem',
+  props: {
+    path: String,
+    activeColor: {
+      type: String,
+      default: 'hotpink'
+    }
+  },
+  // data() {
+  //   return{
+  //     isActive: true
+  //   }
+  // },
+  computed:{
+    isActive() {
+      // indexOf条件不匹配时值为-1
+      return this.$route.path.indexOf(this.path) !== -1
+    },
+    activeStyle() {
+      return this.isActive ? {color: this.activeColor} : {}
+    }
+  },
+  methods: {
+    itemClick() {
+      this.$router.replace(this.path)
+    }
+  }
+}
+</script>
+
+<style>
+  .tab-bar-items {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+    font-size: 14px;
+  }
+  .tab-bar-items img {
+    width: 24px;
+    height: 24px;
+    margin-top: 3px;
+    margin-bottom: 2px;
+    vertical-align: middle;
+  }
+  /* .active {
+    color: hotpink;
+  } */
+</style>
---
+++ vue.config.js
@@ -0,0 +1,31 @@
+// const { resolve } = require("core-js/fn/promise")
+
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'components': '@/components',
+        'content': 'components/content',
+        'common': 'components/common',
+        'assets': '@/assets',
+        'network': '@/network',
+        'views': '@/views',
+      }
+    }
+  }
+}
+
+// const path = require('path')
+// const resolve = dir => path.join(__dirname, dir)
+
+// module.exports = {
+//   chainWebpack: config => {
+//     // 设置快捷路径， @ 表示 'src' ，components 表示 'src/components'
+//     config.resolve.alias
+//       .set('@', resolve('src'))
+//       .set('assets', resolve('src/assets'))
+//       .set('components', resolve('src/components'))
+//       .set('views', resolve('src/views'))
+//       .set('network', resolve('src/network'))
+//   },
+// }
